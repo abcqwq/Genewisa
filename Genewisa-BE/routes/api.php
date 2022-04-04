e4b6f889efb3484127f9a49b4ef585f864e46c75
Joshua Erlangga Sakti <57672096+joshuagatizz@users.noreply.github.com>
2022-04-04 16:23:52 +0700

    added migration, model, and controller for TempatWisata

--- Genewisa-BE/routes/api.php
+++ Genewisa-BE/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TempatWisataController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Tempat Wisata
+Route::post('/tempat-wisata', [TempatWisataController::class, 'store']);
+Route::get('/tempat-wisata', [TempatWisataController::class, 'index']);
+Route::get('/tempat-wisata/{id}', [TempatWisataController::class, 'view']);
+Route::put('/tempat-wisata/{id}', [TempatWisataController::class, 'update']);
+Route::delete('/tempat-wisata/{id}', [TempatWisataController::class, 'delete']);
+
+// Review
+Route::post('/review', [ReviewController::class, 'store']);
+Route::get('/review', [ReviewController::class, 'index']);
+Route::get('/review/{id}', [ReviewController::class, 'view']);
+Route::put('/review/{id}', [ReviewController::class, 'update']);
+Route::delete('/review/{id}', [ReviewController::class, 'delete']);
