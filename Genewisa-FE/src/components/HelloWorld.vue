b49e3b92e41730844a856848fc801aba4ae82c91
Butrahandisya <butrahandisya@gmail.com>
2022-04-04 10:44:15 +0700

    installed Pinia, state management for Vue, and TailwindCSS

--- Genewisa-FE/src/components/HelloWorld.vue
+++ Genewisa-FE/src/components/HelloWorld.vue
@@ -1,52 +1,29 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import {onMounted, ref} from 'vue'
 
-defineProps<{ msg: string }>()
+// reactive state
+let count = ref(0)
 
-const count = ref(0)
+// functions that mutate state and trigger updates
+function increment() {
+    count.value++
+}
+
+// lifecycle hooks
+onMounted(() => {
+    console.log(`The initial count is ${count.value}.`)
+})
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+<div>
+    <button @click="increment()" class="bg-slate-800 rounded-full py-2 px-12 w-fit text-white">
+        {{ count }}
+    </button>
+</div>
 </template>
 
 <style scoped>
-a {
-  color: #42b983;
-}
 
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
 </style>
