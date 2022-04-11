75e1164ed8a0b7c28c7011f7bdb1ab928c5ae266
kevinusmayadhyw <57991938+kevinusmayadhyw@users.noreply.github.com>
2022-04-11 12:30:36 +0700

    maked faker using bahasa

--- Genewisa-BE/database/seeders/DatabaseSeeder.php
+++ Genewisa-BE/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(50)->create();
     }
 }
