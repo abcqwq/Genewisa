a0bc26b31309e1a8e12e8ba430565b2933f2a210
Joshua Erlangga Sakti <joshua.sakti@gdn-commerce.com>
2022-05-21 14:33:05 +0700

    created directories for future use"

---
+++ genewisa_flutter/lib/view/home_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+    );
+  }
+}
