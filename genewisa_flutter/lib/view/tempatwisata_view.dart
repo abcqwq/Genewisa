e8bc94ab6da385d56d68b304d700f8b8a4caa521
kevinusmayadhyw <57991938+kevinusmayadhyw@users.noreply.github.com>
2022-06-16 14:27:47 +0700

    Added search

--- genewisa_flutter/lib/view/tempatwisata_view.dart
+++ genewisa_flutter/lib/view/tempatwisata_view.dart
@@ -10,64 +10,82 @@ class TempatWisataView extends StatefulWidget {
 }
 
 class _TempatWisataViewState extends State<TempatWisataView> {
+
+  final List<Map<String, dynamic>> _allWisata = [
+    {"nama": "Wisata 1", "url": "https://cdn.discordapp.com/attachments/976696861207433219/984030204844453888/Rectangle_3.jpg", "lokasi": "Bandung, Jawa Barat", "rating": 5 },
+    {"nama": "Wisata 2", "url": "https://cdn.discordapp.com/attachments/976696861207433219/984030204844453888/Rectangle_3.jpg", "lokasi": "Bandung, Jawa Barat", "rating": 5 },
+    {"nama": "Wisata 3", "url": "https://cdn.discordapp.com/attachments/976696861207433219/984030204844453888/Rectangle_3.jpg", "lokasi": "Bogor, Jawa Barat", "rating": 5 },
+    {"nama": "Wisata 4", "url": "https://cdn.discordapp.com/attachments/976696861207433219/984030204844453888/Rectangle_3.jpg", "lokasi": "Bandung, Jawa Barat", "rating": 5 },
+    {"nama": "Wisata 5", "url": "https://cdn.discordapp.com/attachments/976696861207433219/984030204844453888/Rectangle_3.jpg", "lokasi": "Bandung, Jawa Barat", "rating": 5 },
+  ];
+
+  List<Map<String, dynamic>> _foundWisata = [];
+  @override
+  initState() {
+    _foundWisata = _allWisata;
+    super.initState();
+  }
+
+  void _runFilter(String enteredKeyword) {
+    List<Map<String, dynamic>> results = [];
+    if (enteredKeyword.isEmpty) {
+      results = _allWisata;
+    } else {
+      results = _allWisata
+          .where((wisata) =>
+              wisata["nama"].toLowerCase().contains(enteredKeyword.toLowerCase()) || wisata["lokasi"].toLowerCase().contains(enteredKeyword.toLowerCase()))
+          .toList();
+    }
+
+    setState(() {
+      _foundWisata = results;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
-        padding: EdgeInsets.only(left: 30,right: 30, top: 20),
-        child: Align(
-          alignment: Alignment.topCenter,
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  margin: EdgeInsets.only(bottom: 20, top: 5),
-                  child: TextField(
-                    decoration: const InputDecoration(
-                        prefixIcon: Icon(Icons.search),
-                        labelText: 'Search', 
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                        ),
-                    ),
-                  ),
-                ),
-                ListWisataContainer(
-                  nama: "Wisata 1",
-                  lokasi: "Bandung, Jawa Barat",
-                  url: "https://cdn.discordapp.com/attachments/976696861207433219/984030204844453888/Rectangle_3.jpg",
-                  rating: 5,
-                ),
-                ListWisataContainer(
-                  nama: "Wisata 2",
-                  lokasi: "Bogor, Jawa Barat",
-                  url: "https://cdn.discordapp.com/attachments/976696861207433219/984030204844453888/Rectangle_3.jpg",
-                  rating: 5,
-                ),
-                ListWisataContainer(
-                  nama: "Wisata 3",
-                  lokasi: "Bandung, Jawa Barat",
-                  url: "https://cdn.discordapp.com/attachments/976696861207433219/984030204844453888/Rectangle_3.jpg",
-                  rating: 5,
-                ),
-                ListWisataContainer(
-                  nama: "Wisata 4",
-                  lokasi: "Bogor, Jawa Barat",
-                  url: "https://cdn.discordapp.com/attachments/976696861207433219/984030204844453888/Rectangle_3.jpg",
-                  rating: 5,
-                ),
-                ListWisataContainer(
-                  nama: "Wisata 5",
-                  lokasi: "Bogor, Jawa Barat",
-                  url: "https://cdn.discordapp.com/attachments/976696861207433219/984030204844453888/Rectangle_3.jpg",
-                  rating: 5,
-                ),
-              ],
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
             ),
-          ),
-      ),
+            TextField(
+              onChanged: (value) => _runFilter(value),
+              decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      labelText: 'Search',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      ),
+                    ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: _foundWisata.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: _foundWisata.length,
+                      itemBuilder: (context, index) => ListWisataContainer(
+                      nama: _foundWisata[index]["nama"].toString(),
+                      lokasi: _foundWisata[index]["lokasi"].toString(),
+                      url: _foundWisata[index]["url"].toString(),
+                      rating: _foundWisata[index]["rating"],
+                    )
+                    )
+                  : const Text(
+                      'Pencarian tidak ditemukan',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+            ),
+          ],
+        ),
       ),
     );
   }
